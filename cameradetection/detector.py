177416a014a4a2020e90050e5ced66b1a23c7b54
gimicaroni <gimicaroni@gmail.com>
2024-07-04 11:27:41 -0300

    ruido calculado na imagem toda e transformacao para vetor feita

--- cameradetection/detector.py
+++ cameradetection/detector.py
@@ -2,33 +2,35 @@ import numpy as np
 from PIL import Image
 import random
 import pywt
-from skimage.restoration import denoise_wavelet
+from sklearn.cluster import KMeans
+
+PATCH_SIZE = 100
+N_PATCHES = 15
 
 # Function to extract a certain number of patches of a certain size from an image
-def extract_subimages(image_path, subimage_size = [100, 100], N=15):
+def extract_patches(img_array, patch_size = [PATCH_SIZE, PATCH_SIZE], N=N_PATCHES):
     # Open the image and convert to a NumPy array
-    img = Image.open(image_path)
-    img_array = np.array(img)
+
     img_height, img_width, _ = img_array.shape
     
-    subimages = []
-    subimages_array = []
+    patches = []
+    patches_array = []
     
     for _ in range(N):
         # Generate random coordinates for the top-left corner of the subimage
-        x = random.randint(0, img_width - subimage_size[0])
-        y = random.randint(0, img_height - subimage_size[1])
+        x = random.randint(0, img_width - patch_size[0])
+        y = random.randint(0, img_height - patch_size[1])
         
         # Extract the subimage using NumPy slicing
-        subimage_array = img_array[y:y + subimage_size[1], x:x + subimage_size[0]]
-        subimages_array.append(subimage_array)
+        patch_array = img_array[y:y + patch_size[1], x:x + patch_size[0]]
+        patches_array.append(patch_array)
         
         # Convert the subimage array back to a Pillow Image
-        subimage = Image.fromarray(subimage_array)
+        patch = Image.fromarray(patch_array)
         
-        subimages.append(subimage)
+        patches.append(patch)
     
-    return subimages, subimages_array
+    return patches, patches_array
 
 # Function to perform wavelet denoising on a single channel
 def wavelet_denoising(channel, wavelet='db1', level=1, threshold=0.04):
@@ -65,27 +67,26 @@ def denoise_rgb(image_array):
 
         return denoised_image_array
 
+# def optimize_kmeans(data, max_k):
+     
+
 if __name__ == '__main__':
     image_path = 'm.010lz5_0001.jpg'
-    subimages, subimages_array = extract_subimages(image_path)
-    noise_matrix = []
-    for count, image_array in enumerate(subimages_array):
-        denoised_image = denoise_rgb(image_array)
-        print(denoised_image.shape)
-        denoised_image_jpg = Image.fromarray(denoised_image)
-        denoised_image_jpg.show()
-        image_array_jpg = Image.fromarray(image_array)
-        image_array_jpg.show()
-        noise = image_array - denoised_image
-        noise_matrix.append(noise)
-    noise_matrix = np.array(noise_matrix) #matrix with shape (15, 100, 100, 3)
-    avg_noise_matrix = np.mean(noise_matrix, axis=0) #matrix with the average noise of the 15 
-    # img = Image.fromarray(avg_noise_matrix)
-    # img.show()
-    print(denoised_image.shape)
+    image = Image.open(image_path)
+    image_array = np.array(image)
 
+    denoise_image_array = denoise_rgb(image_array)
+    noise = image_array - denoise_image_array
+    patches, patches_array = extract_patches(noise)
+
+    avg_noise_matrix = np.mean(patches_array, axis=0) #matrix with the average noise of the 15 patches
     print(avg_noise_matrix.shape)
 
+    noise_vector = []
+    for i in range(PATCH_SIZE):
+         noise_vector.append(0.299*avg_noise_matrix[i][i][0] + 0.587*avg_noise_matrix[i][i][1] + 0.114*avg_noise_matrix[i][i][2]) 
+    noise_vector = np.array(noise_vector)
+    print(noise_vector.shape)
 
     
     
