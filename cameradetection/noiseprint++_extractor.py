1976510b8e8475d0e2804622cdcebd21d2f1f3c7
gimicaroni <gimicaroni@gmail.com>
2024-07-25 16:18:51 -0300

    printa que concluiu

--- cameradetection/noiseprint++_extractor.py
+++ cameradetection/noiseprint++_extractor.py
@@ -21,9 +21,7 @@ if __name__ == '__main__':
         lista_noises.append(noise)
 
     array_noises = np.array(lista_noises) #array_noises stores the values of the noises of all images
-    print(array_noises.shape)
     avg_noise_array = np.mean(array_noises, axis=0) #calculates the mean of the noises of all images
     pooled_vector = np.mean(avg_noise_array, axis=1) #applies an average pooling to the mean of the noises of all images to obtain a vector
-    print(avg_noise_array.shape)
-    print(pooled_vector.shape)
     np.save('np++5cameras.npy', avg_noise_array)
+    print('Array saved as np++5cameras.npy')
