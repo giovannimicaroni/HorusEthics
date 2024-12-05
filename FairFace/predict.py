0942af64d243c901357411ac562fb4d9b95e0d0b
emilytomadon <emilytomadon@gmail.com>
2024-12-05 12:00:52 -0300

    add faircal ao repositório

--- FairFace/predict.py
+++ FairFace/predict.py
@@ -59,17 +59,17 @@ def detect_face(image_paths,  SAVE_DETECTED_AT, default_max_size=800,size = 300,
 
 def predidct_age_gender_race(save_prediction_at, imgs_path = 'cropped_faces/'):
     img_names = [os.path.join(imgs_path, x) for x in os.listdir(imgs_path)]
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cpu")
 
     model_fair_7 = torchvision.models.resnet34(pretrained=True)
     model_fair_7.fc = nn.Linear(model_fair_7.fc.in_features, 18)
-    model_fair_7.load_state_dict(torch.load('fair_face_models/fairface_alldata_20191111.pt'))
+    model_fair_7.load_state_dict(torch.load('fair_face_models/res34_fair_align_multi_7_20190809.pt', map_location=device))
     model_fair_7 = model_fair_7.to(device)
     model_fair_7.eval()
 
     model_fair_4 = torchvision.models.resnet34(pretrained=True)
     model_fair_4.fc = nn.Linear(model_fair_4.fc.in_features, 18)
-    model_fair_4.load_state_dict(torch.load('fair_face_models/fairface_alldata_4race_20191111.pt'))
+    model_fair_4.load_state_dict(torch.load('fair_face_models/res34_fair_align_multi_4_20190809.pt', map_location=device))
     model_fair_4 = model_fair_4.to(device)
     model_fair_4.eval()
 
@@ -215,4 +215,4 @@ if __name__ == "__main__":
     detect_face(imgs, SAVE_DETECTED_AT)
     print("detected faces are saved at ", SAVE_DETECTED_AT)
     #Please change test_outputs.csv to actual name of output csv. 
-    predidct_age_gender_race("test_outputs.csv", SAVE_DETECTED_AT)
+    predidct_age_gender_race("meus_resultados.csv", SAVE_DETECTED_AT)
--- FairFace/predict_bbox.py
+++ FairFace/predict_bbox.py
@@ -80,14 +80,14 @@ def predidct_age_gender_race(save_prediction_at, bboxes, imgs_path = 'cropped_fa
 
     model_fair_7 = torchvision.models.resnet34(pretrained=True)
     model_fair_7.fc = nn.Linear(model_fair_7.fc.in_features, 18)
-    model_fair_7.load_state_dict(torch.load('fair_face_models/fairface_alldata_20191111.pt'))
+    model_fair_7.load_state_dict(torch.load('fair_face_models/res34_fair_align_multi_7_20190809.pt', map_location=device))
     #model_fair_7.load_state_dict(torch.load('fair_face_models/res34_fair_align_multi_7_20190809.pt'))
     model_fair_7 = model_fair_7.to(device)
     model_fair_7.eval()
 
     model_fair_4 = torchvision.models.resnet34(pretrained=True)
     model_fair_4.fc = nn.Linear(model_fair_4.fc.in_features, 18)
-    model_fair_4.load_state_dict(torch.load('fair_face_models/fairface_alldata_4race_20191111.pt'))
+    model_fair_4.load_state_dict(torch.load('fair_face_models/res34_fair_align_multi_4_20190809.pt', map_location=device))
     model_fair_4 = model_fair_4.to(device)
     model_fair_4.eval()
 
@@ -236,4 +236,4 @@ if __name__ == "__main__":
     imgs = pd.read_csv(args.input_csv)['img_path']
     bboxes = detect_face(imgs, SAVE_DETECTED_AT)
     print("detected faces are saved at ", SAVE_DETECTED_AT)
-    predidct_age_gender_race("test_outputs.csv", bboxes, SAVE_DETECTED_AT)
+    predidct_age_gender_race("resultados_bbox.csv", bboxes, SAVE_DETECTED_AT)
